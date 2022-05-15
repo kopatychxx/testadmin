61feaf24ca99c2bdf85bcbd4b5aca475ec4c2645
kopatychxx <evgenijsalo88@gmail.com>
2022-05-15 21:41:49 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,122 +1,48 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="login">
+    <img src="../assets/logo.png" alt="" width="92" />
+    <h3>Войдите В Админку!</h3>
+    <div class="text">
+      Используй логин "admin" и пароль "admin" что бы войти!
+    </div>
+    <div class="inputs">
+      <v-text-field label="Логин" hide-details="auto"></v-text-field>
+      <v-text-field
+        label="Пароль"
+        :rules="rules"
+        hide-details="auto"
+        type="password"
+      ></v-text-field>
+      <v-btn><router-link to="/about">Войти</router-link></v-btn>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  data: () => ({
+    rules: [
+      (value) => !!value || "Что-то пошло не так.",
+      (value) => value === "admin",
+    ],
+  }),
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+<style>
+.login {
+  margin: 200px auto;
+  padding: 100px;
+  max-width: 500px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.inputs {
+  display: flex;
+  flex-direction: column;
+  row-gap: 30px;
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,213 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="statistic">
+      <div class="allFlat">
+        <h4>Всего квартир</h4>
+        <p><span>300</span>квартир</p>
+      </div>
+      <div class="allFlat">
+        <h4>Проданно квартир</h4>
+        <div class="card_content">
+          <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="valueOne"
+            color="white"
+          >
+            {{ valueOne }} %
+          </v-progress-circular>
+          <p><span>120</span>квартир</p>
+        </div>
+      </div>
+      <div class="allFlat">
+        <h4>Просмотр квартир</h4>
+        <div class="card_content">
+          <v-progress-circular
+            :rotate="360"
+            :size="100"
+            :width="15"
+            :value="valueTwo"
+            color="white"
+          >
+            {{ valueTwo }} %
+          </v-progress-circular>
+          <p><span>160</span>квартир</p>
+        </div>
+      </div>
+    </div>
+    <div class="filter">
+      <div class="idtest">
+        <v-text-field label="Id" type="number"></v-text-field>
+      </div>
+      <p class="nameFlat">Название</p>
+      <div class="testCout">
+        <v-select
+          v-model="e7"
+          :items="states"
+          label="Комнат"
+          multiple
+          chips
+          persistent-hint
+        ></v-select>
+      </div>
+      <div class="testStatus">
+        <v-select
+          v-model="e7"
+          :items="status"
+          label="Статус"
+          multiple
+          chips
+          persistent-hint
+        ></v-select>
+      </div>
+      <div class="testPrice" style="width: 12%">
+        <div class="valueMinMax">
+          <v-text-field label="От" type="number"></v-text-field>
+        </div>
+        <span>-</span>
+        <div class="valueMinMax">
+          <v-text-field label="до" type="number"></v-text-field>
+        </div>
+      </div>
+      <p>Подробней</p>
+    </div>
+    <div class="table" v-for="flat in flats" :key="flat.id">
+      <p class="idTable">{{ flat.id }}</p>
+      <p class="nameFlat">{{ flat.name }}</p>
+      <p class="testCout">{{ flat.room }} комната</p>
+      <p class="testStatus">{{ flat.status }}</p>
+      <p class="testPrice">{{ flat.price }} грн</p>
+      <v-btn elevation="0" style="padding: 3px">Больше</v-btn>
+    </div>
   </div>
 </template>
+<script>
+export default {
+  name: "AboutView",
+  data() {
+    return {
+      e7: [],
+      states: ["1 комната", "2 комнаты", "3 комнаты", "4 комнаты"],
+      status: ["Свободная", "Просмотр", "Проданна"],
+      interval: {},
+      valueOne: 40,
+      valueTwo: 60,
+      flats: [
+        {
+          id: 1,
+          name: "Однокомнатная квартира",
+          room: 1,
+          description: "Тут будет описание",
+          status: "Свободная",
+          price: 10000,
+        },
+        {
+          id: 2,
+          name: "Двух комнатная квартира",
+          room: 2,
+          description: "Тут будет описание",
+          status: "Просмотр",
+          price: 10000,
+        },
+        {
+          id: 3,
+          name: "Трехкомнатная квартира",
+          room: 3,
+          description: "Тут будет описание",
+          status: "Просмотр",
+          price: 10000,
+        },
+        {
+          id: 4,
+          name: "Четырехкомнатрая квартира",
+          room: 4,
+          description: "Тут будет описание",
+          status: "Свободная",
+          price: 10000,
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+.about {
+  margin: 10px 100px;
+}
+.filter {
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  column-gap: 20px;
+  align-items: flex-end;
+}
+.table {
+  border-radius: 4px;
+  display: flex;
+  column-gap: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin-bottom: 20px;
+  padding: 0 5px;
+}
+.statistic {
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 50px;
+}
+.allFlat {
+  background: #36452f;
+  box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.25);
+  border-radius: 4px;
+  width: 500px;
+  height: 200px;
+  padding: 20px 30px;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+}
+.allFlat h4 {
+  font-weight: 700;
+  font-size: 35px;
+  color: #e08652;
+}
+.allFlat span {
+  font-weight: 700;
+  font-size: 50px;
+  color: #e08652;
+  margin-right: 15px;
+}
+.allFlat p {
+  margin: 0;
+  font-size: 20px;
+  color: white;
+}
+.card_content {
+  display: flex;
+  align-items: flex-end;
+  column-gap: 20px;
+}
+.idtest,
+.idTable {
+  width: 150px;
+}
+.nameFlat {
+  width: 300px;
+}
+.testCout {
+  width: 440px;
+}
+.testStatus {
+  width: 420px;
+}
+.testPrice {
+  display: flex;
+  flex-direction: row;
+  column-gap: 20px;
+  align-items: center;
+  width: 12%;
+}
+.valueMinMax {
+  width: 80px;
+}
+</style>
